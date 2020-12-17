1aea726b26cb16446fdb4bd3453e7848661641bc
Anu2107 <73894555+Anu2107@users.noreply.github.com>
2020-12-17 19:49:21 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,37 @@
+class Paper{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options); 
+		this.paperImage = loadImage("paper.png"); 
+		World.add(world, this.body);
+
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rotate(this.body.angle); 
+			imageMode(CENTER)
+			//strokeWeight(4);
+			fill(255)
+			image(this.paperImage, 0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
